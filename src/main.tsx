ca577fa846f04a21cee3a5290d616c72734a3928
Allison Segrera Pardo <paulethsegrera@gmail.com>
2025-11-18 08:32:37 -0500

    fix: usar HashRouter para GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { HashRouter } from 'react-router-dom'   // ← cambiaste BrowserRouter por HashRouter
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>                                // ← aquí también
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
